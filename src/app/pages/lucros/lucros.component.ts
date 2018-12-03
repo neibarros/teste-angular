2eadab0bf50f67c94e1195c6dfbaf5eac4f01e45
Nei Barros <nei_webdev@hotmail.com>
2018-12-03 04:56:22 -0200

    Improving LucroComponent

--- src/app/pages/lucros/lucros.component.ts
+++ src/app/pages/lucros/lucros.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { LucroService } from './shared/lucro.service';
+import { Lucro } from './shared/lucro.model';
+
 @Component({
 	selector: 'app-lucros',
 	templateUrl: './lucros.component.html',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LucrosComponent implements OnInit {
 
-	constructor() { }
+	lucros: Lucro[] = [];
+
+	constructor(
+		private lucroService: LucroService
+	) { }
 
 	ngOnInit() {
+		this.get();
 	}
 
+	get() {
+		this.lucroService.getAll().subscribe(
+			lucros => {
+				this.lucros = lucros.sort((a, b) => {
+					if (a.dateMoviment < b.dateMoviment) {
+						return 1;
+					}
+					if (a.dateMoviment > b.dateMoviment) {
+						return -1;
+					}
+					return 0;
+				});
+				console.log(this.lucros);
+			},
+			error => alert('Erro ao carregar a lista')
+		);
+	}
 }
